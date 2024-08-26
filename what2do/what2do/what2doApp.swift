1aa0d18c95aca80cbb44dbff441fa7c5eaa6ee08
guillemsarda <gsardaparreu@gmail.com>
2024-08-26 18:16:13 +0200

    build(ios): create swiftui project

---
+++ what2do/what2do/what2doApp.swift
@@ -0,0 +1,17 @@
+//
+//  what2doApp.swift
+//  what2do
+//
+//  Created by Guillem Sardà Parreu on 26/8/24.
+//
+
+import SwiftUI
+
+@main
+struct what2doApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
+    }
+}
