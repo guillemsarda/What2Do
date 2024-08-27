6c037656ac833771e5c29131d934d25b81696974
guillemsarda <gsardaparreu@gmail.com>
2024-08-27 11:48:01 +0200

    feat: improve welcome view

---
+++ what2do/what2do/Screens/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  what2do
+//
+//  Created by Guillem Sardà Parreu on 27/8/24.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
---
+++ what2do/what2do/Screens/WelcomeView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  what2do
+//
+//  Created by Guillem Sardà Parreu on 26/8/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        ZStack {
+            Text(LocalizedStringKey("welcome"))
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .zIndex(1)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                .padding([.top], 30)
+            VStack {
+                Text(LocalizedStringKey("accountQuestion"))
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .padding([.bottom], 30)
+                    .multilineTextAlignment(.center)
+                Button(LocalizedStringKey("accountAnswerPositive"), action: {
+                    print("hello")
+                })
+                .foregroundColor(.black)
+                .padding([.bottom, .top], 30)
+                .padding([.leading, .trailing], 50)
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color("LightBlue"))
+                )
+                .padding([.bottom], 15)
+                Button(LocalizedStringKey("accountAnswerNegative"), action: {
+                    print("hello")
+                })
+                .foregroundColor(.black)
+                .padding([.bottom, .top], 30)
+                .padding([.leading, .trailing], 50)
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color("SeaBlue"))
+                )
+            }
+            .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, minHeight: 0, maxHeight:.infinity)
+        }.zIndex(5)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
